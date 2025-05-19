d8a94cb44c4b4691c5f1e96c5b7f4c2a61ae7b7d
Hemat-ATOM <hematullahw@gmail.com>
2025-05-20 00:26:11 +0430

    Used and handled chats data to FileSection & MemberList components

--- src/components/FileSection.jsx
+++ src/components/FileSection.jsx
@@ -1,48 +1,85 @@
 import React from "react";
 import { Accordion, AccordionItem } from "@heroui/accordion";
 import { Card, CardBody, CardHeader } from "@heroui/card";
+import getFileURL from "../utils/setFileURL";
 
-
-function FileSection({ files }) {
+function FileSection({ data }) {
   return (
     <Card className="bg-white dark:bg-dark shadow-lg mt-4">
       <CardHeader>Files</CardHeader>
       <CardBody>
         <Accordion selectionMode="multiple">
-          {files.map((file) => {
-            switch (file.type) {
-              case "photos":
-                return (
-                  <AccordionItem
-                    // key="1"
-                    aria-label="Accordion 1"
-                    title={file.count + " Photos"}
+          <AccordionItem
+            // key="1"
+            aria-label="Accordion 1"
+            title={data?.media.length + " Media"}
+          >
+            {
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
+                {data?.media.map((file) => (
+                  <div
+                    key={file._id}
+                    className="w-16 border rounded-md bg-default-300 shadow aspect-square"
                   >
-                    {"ABC"}
-                  </AccordionItem>
-                );
-              case "files":
-                return (
-                  <AccordionItem
-                    // key="1"
-                    aria-label="Accordion 1"
-                    title={file.count + " Files"}
-                  >
-                    {"ABC"}
-                  </AccordionItem>
-                );
-              case "shared links":
-                return (
-                  <AccordionItem
-                    // key="1"
-                    aria-label="Accordion 1"
-                    title={file.count + " Shared Links"}
-                  >
-                    {"ABC"}
-                  </AccordionItem>
-                );
+                    {file.mediaType === "image" && (
+                      <img
+                        src={getFileURL(file.path)}
+                        alt="media"
+                        className="w-full h-auto rounded-md object-cover aspect-square"
+                      />
+                    )}
+                    {file.mediaType === "audio" && (
+                      <audio controls className="w-full mt-2">
+                        <source src={getFileURL(file.path)} type="audio/webm" />
+                        Your browser does not support the audio element.
+                      </audio>
+                    )}
+                    {file.mediaType === "video" && (
+                      <video
+                        controls
+                        className="w-full h-auto rounded-md aspect-square"
+                        style={{ maxHeight: "300px" }}
+                      >
+                        <source src={getFileURL(file.path)} type="video/webm" />
+                        Your browser does not support the video tag.
+                      </video>
+                    )}
+                  </div>
+                ))}
+              </div>
             }
-          })}
+          </AccordionItem>
+          <AccordionItem
+            // key="1"
+            aria-label="Accordion 1"
+            title={data?.files.length + " Files"}
+          >
+            <div className="grid grid-cols-3 gap-4">
+              {data?.files.map((file) => {
+                // const fileName =
+                //   file.path.split("/")[file.path.split("/").length - 1];
+                return (
+                  <a
+                    target="_blank"
+                    href={getFileURL(file.path)}
+                    key={file._id}
+                    className="  border rounded-md  shadow flex items-center gap-2"
+                  >
+                    <span className="text-red-400 text-xl grid place-content-center h-16 w-16 bg-white font-bold">PDF</span>
+                    {/* <span className="text-nowrap">{fileName}</span> */}
+                  </a>
+                );
+              })}
+            </div>
+          </AccordionItem>
+          );
+          <AccordionItem
+            // key="1"
+            aria-label="Accordion 1"
+            title={data?.links.length + " Shared Links"}
+          >
+            {"ABC"}
+          </AccordionItem>
         </Accordion>
       </CardBody>
     </Card>
--- src/components/MemberList.jsx
+++ src/components/MemberList.jsx
@@ -1,20 +1,52 @@
 import React from "react";
 import { Card, CardBody, CardHeader } from "@heroui/card";
 import { User } from "@heroui/user";
+import { useQuery } from "@tanstack/react-query";
+import axiosIns from "../utils/axios";
+import getFileURL from "../utils/setFileURL";
+import { useAuth } from "../contexts/AuthContext";
 
-function MemberList({ members }) {
+function MemberList({ members, selectedChat }) {
+  const { user } = useAuth();
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["chats", selectedChat?._id, "members"],
+    queryFn: async () => {
+      const { data } = await axiosIns.get(
+        `/chats/${selectedChat?._id}/members`
+      );
+      return data;
+    },
+    enabled: !!selectedChat?._id,
+  });
+
+  if (error) {
+    console.log(error);
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Card className="bg-white dark:bg-dark shadow-lg">
       <CardHeader>Members</CardHeader>
       <CardBody>
-        {members.map((member, index) => (
+        {data?.members.map((member, index) => (
           <User
             key={index}
             className="justify-stretch py-1"
             avatarProps={{
-              src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+              src: getFileURL(member.profile),
             }}
-            name={member.name}
+            name={
+              user._id === member._id
+                ? "You"
+                : member.firstName
+                ? member.firstName + " " + member.lastName
+                : member.username
+            }
+            description={data.groupAdmin.includes(member._id) && "Admin"}
             classNames={{
               base: "hover:bg-gray-100 dark:hover:bg-dark-2 transition-all duration-200",
             }}
